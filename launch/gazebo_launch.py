9778193ddcf88c101f280221187f9a38409cb54d
khemanth18 <hemanth@phasesensitiveinc.com>
2026-06-15 19:29:37 +0000

    Gazebo realization error fix

--- launch/gazebo_launch.py
+++ launch/gazebo_launch.py
@@ -3,28 +3,29 @@ from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
+import xacro
 
 def generate_launch_description():
     pkg = get_package_share_directory('sensor_sim')
     urdf_file = os.path.join(pkg, 'urdf', 'robot.urdf.xacro')
 
+    # xacro process பண்றோம்!
+    robot_description = xacro.process_file(urdf_file).toxml()
+
     return LaunchDescription([
-        # Gazebo start பண்றது
         ExecuteProcess(
             cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
             output='screen'
         ),
 
-        # Robot state publisher
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[{'robot_description': open(urdf_file).read()}]
+            parameters=[{'robot_description': robot_description}]
         ),
 
-        # Robot spawn பண்றது
         Node(
             package='gazebo_ros',
             executable='spawn_entity.py',
@@ -32,4 +33,3 @@ def generate_launch_description():
             output='screen'
         ),
     ])
-    
